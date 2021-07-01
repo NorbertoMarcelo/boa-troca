53d9279d388d954eed8f442f2fdba67038049ff8
Marcelo Norberto <norbertomarcelo8@gmail.com>
2021-07-01 20:58:21 -0300

    Create database connection

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 const port = 3333;
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello!!' });
-});
+app.use(router);
 
 app.listen(port, () =>
   console.log(`Server listening at http://localhost:${port}`)
